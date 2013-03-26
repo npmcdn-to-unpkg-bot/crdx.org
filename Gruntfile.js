ad664420828b57002e1eba6d9e272ff91a8d6fa0
crdx <git@crdx.org>
2013-03-26 00:01:54 +0000

    add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-shell");
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+
+        less: {
+            dist: {
+                files: {
+                    "css/style.css": "css/style.less" /* out: in */
+                },
+            },
+        },
+
+        watch: {
+            files: ["**/*", "!node_modules/**/*"],
+            tasks: ["jshint", "less:dist", "jekyll"]
+        },
+
+        jshint: {
+            Gruntfile: {
+                options: {
+                    es5: true,
+                },
+                files: {
+                    src: ["Gruntfile.js"],
+                },
+            },
+
+            js: {
+                options: {
+                    laxbreak: true
+                },
+                files: {
+                    src: ["js/{my,github-commits,cookie-generator,analytics}.js"]
+                },
+            },
+        },
+    });
+
+    grunt.registerTask("jekyll", "generates jekyll", function() {
+        var done = this.async();
+
+        grunt.util.spawn({ cmd: "jekyll" }, function(error, result, code) {
+            if (error)
+            {
+                grunt.log.error(error);
+                done(false);
+            }
+            else
+            {
+                grunt.log.writeln(result);
+                done(true);
+            }
+        });
+    });
+
+    grunt.registerTask("deploy", function() {
+
+    });
+
+    grunt.registerTask("default", ["watch"]);
+};
