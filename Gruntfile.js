5db7a57d987aaf88e7bc39cf742850f985ea2883
crdx <git@crdx.org>
2013-04-10 19:44:06 +0100

    minify js and css and combine them into one file

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,30 +2,51 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-contrib-less");
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
 
+        uglify: {
+            all: {
+                files: {
+                    "js/all.min.js": [
+                        "js/html5shiv.js",
+                        "js/bootstrap.min.js", // todo use non-minified versions of these
+                        "js/util.js",
+                        "js/cookie-generator.js",
+                        "js/github-commits.js"
+                    ]
+                },
+            },
+        },
+
+        cssmin: {
+            all: {
+                files: {
+                    "css/style.min.css": [
+                        "css/bootstrap.min.css",
+                        "css/bootstrap-responsive.min.css",
+                        "css/glyphicons.css",
+                        "css/syntax.css",
+                        "css/style.css",
+                    ]
+                },
+            },
+        },
+
         less: {
-            dist: {
+            all: {
                 files: {
                     "css/style.css": "css/style.less"
-                },
-                options: {
-                    yuicompress: true
                 }
             },
-
-            dev: {
-                files: {
-                    "css/style.css": "css/style.less"
-                }
-            }
         },
 
         watch: {
             files: ["**/*", "!node_modules/**/*"],
-            tasks: ["jshint", "less:dev", "jekyll"]
+            tasks: ["jshint", "uglify", "less", "cssmin", "jekyll"]
         },
 
         jshint: {
